c26aaab13c12b85a225711f8e1614572d9ceeaa0
amiits <squerart_web@interia.pl>
2009-02-13 13:04:04 -0700

    Spelling mistake

--- apps/game/modules/settlement/templates/foundSuccess.php
+++ apps/game/modules/settlement/templates/foundSuccess.php
@@ -1,6 +1,6 @@
 <h3>Found new settlement dependent on <?php echo $settlement->name; ?></h3>
 <p>Main settlement position: 0, 0</p>
-<p>Type new settlement position. Remember, that distance cannot be longer than 7 fields (optimal) and shorten than 3 fields.</p>
+<p>Type new settlement position. Remember, that distance cannot be longer than 7 fields (optimal) and shorter than 3 fields.</p>
 <p>
   
   <form action="<?php echo url_for("@found_settlement?id={$sf_request->getParameter('id')}") ?>" method="POST">
